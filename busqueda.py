6dd68154521db48badb649fbe7ccab5f74f8fe98
nattkmm <129527144+nattkmm@users.noreply.github.com>
2023-06-17 16:20:16 -0400

    combobox de región

--- busqueda.py
+++ busqueda.py
@@ -21,6 +21,34 @@ class ventanadeBusqueda(QMainWindow):
         self.combobox.addItem("VEI")
 
         self.layout.addWidget(self.combobox)
+        self.combobox.currentIndexChanged.connect(self.comboBoxRegion)
+
+    def comboBoxRegion (self, index):
+        while self.layout.count() > 1:
+            item = self.layout.takeAt(1)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater() 
+
+        if index == 0:
+            region_combobox = QComboBox()
+            region_combobox.addItem("Arica y Parinacota")
+            region_combobox.addItem("Tarapacá")
+            region_combobox.addItem("Antofagasta")
+            region_combobox.addItem("Atacama")
+            region_combobox.addItem("Coquimbo")
+            region_combobox.addItem("Valparaíso")
+            region_combobox.addItem("Metropolitana")
+            region_combobox.addItem("O'Higgins")
+            region_combobox.addItem("Maule")
+            region_combobox.addItem("Ñuble")
+            region_combobox.addItem("Biobío")
+            region_combobox.addItem("La Araucanía")
+            region_combobox.addItem("Los Ríos")
+            region_combobox.addItem("Los Lagos")
+            region_combobox.addItem("Aysén")
+            region_combobox.addItem("Magallanes")
+            self.layout.addWidget(region_combobox)
         
     def busquedaManual(self):
         busquedaTexto=self.busquedaTexto_edit.text()
