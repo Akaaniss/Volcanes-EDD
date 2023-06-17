5da6d83965f9638a3e36a876f903fe3cdf6a85ea
Akaaniss <annaismonserrat.skz@gmail.com>
2023-06-17 15:29:44 -0400

    Update busqueda.py

--- busqueda.py
+++ busqueda.py
@@ -1,18 +1,12 @@
+import sys
 import csv
-from PyQt6.QtWidgets import QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
 
 class ventanadeBusqueda(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Busqueda de Volcanes")
-        self.setGeometry(100,100,400,200)
-
-        self.central_widget= QWidget()
-        self.setCentralWidget(self.central_widget)
-
-        self.layout=QVBoxLayout()
-        self.central_widget.setLayout(self.layout)
-        
+        self.setGeometry(300,300,400,300)
 
 
 
@@ -31,3 +25,9 @@ class ventanadeBusqueda(QMainWindow):
 
 
 
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ventanadeBusqueda()
+    window.show()
+    sys.exit(app.exec())
