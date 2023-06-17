d502f64029b2d1f073c64e07df0fd00ae20058aa
Akaaniss <annaismonserrat.skz@gmail.com>
2023-06-17 15:22:48 -0400

    Update busqueda.py

--- busqueda.py
+++ busqueda.py
@@ -6,7 +6,8 @@ class ventanadeBusqueda(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Busqueda de Volcanes")
-        
+        self.setGeometry(100,100,400,200)
+
         self.central_widget= QWidget()
         self.setCentralWidget(self.central_widget)
 
