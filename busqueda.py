af0c89c8f852d6786cbfbf3c1a327c2dceb23f16
Felipealopez <129527148+Felipealopez@users.noreply.github.com>
2023-06-20 21:52:29 -0400

    Add files via upload

--- busqueda.py
+++ busqueda.py
@@ -20,14 +20,14 @@ class VentanaBusqueda(QMainWindow):
         self.combobox = QComboBox(self)
         self.combobox.addItems(["Nombre del volcán", "Región", "Año", "VEI"])
         self.combobox.setCurrentIndex(0)
-        self.combobox.currentIndexChanged.connect(self.combobox_region)
+        self.combobox.currentIndexChanged.connect(self.comboboxRegion)
         self.combobox.move(50, 30)
 
-        self.input_entry = QLineEdit(self)
-        self.input_entry.move(50, 70)
+        self.entradatexto = QLineEdit(self)
+        self.entradatexto.move(50, 70)
 
-        self.region_combobox = QComboBox(self)
-        self.region_combobox.addItems(
+        self.regionCombobox = QComboBox(self)
+        self.regionCombobox.addItems(
             [
                 "Region de Arica y Parinacota",
                 "Region de Tarapaca",
@@ -47,66 +47,66 @@ class VentanaBusqueda(QMainWindow):
                 "Region de Magallanes y la Antartida Chilena",
             ]
         )
-        self.region_combobox.setCurrentIndex(0)
-        self.region_combobox.move(50, 70)
-        self.region_combobox.hide()
+        self.regionCombobox.setCurrentIndex(0)
+        self.regionCombobox.move(50, 70)
+        self.regionCombobox.hide()
 
-        self.search_button = QPushButton("Buscar", self)
-        self.search_button.clicked.connect(self.realizar_busqueda)
-        self.search_button.move(50, 110)
+        self.botonBusqueda = QPushButton("Buscar", self)
+        self.botonBusqueda.clicked.connect(self.BusquedaBinaria)
+        self.botonBusqueda.move(50, 110)
 
-        self.result_table = QTableWidget(self)
-        self.result_table.setGeometry(50, 150, 500, 240)
-        self.result_table.setColumnCount(5)
-        self.result_table.setHorizontalHeaderLabels(["Región", "Nombre del volcán", "Año", "VEI", "Coordenadas"])
+        self.resultadoTabla = QTableWidget(self)
+        self.resultadoTabla.setGeometry(50, 150, 500, 240)
+        self.resultadoTabla.setColumnCount(5)
+        self.resultadoTabla.setHorizontalHeaderLabels(["Región", "Nombre del volcán", "Año", "VEI", "Coordenadas"])
 
-    def combobox_region(self, index):
+    def comboboxRegion(self, index):
         if index == 1:
-            self.input_entry.hide()
-            self.region_combobox.show()
+            self.entradatexto.hide()
+            self.regionCombobox.show()
         else:
-            self.region_combobox.hide()
-            self.input_entry.show()
+            self.regionCombobox.hide()
+            self.entradatexto.show()
 
-    def realizar_busqueda(self):
-        criteria = self.combobox.currentText()
-        if criteria == "Región":
-            value = self.region_combobox.currentText()
+    def BusquedaBinaria(self):
+        criterio = self.combobox.currentText()
+        if criterio == "Región":
+            valor = self.regionCombobox.currentText()
         else:
-            value = self.input_entry.text()
+            valor = self.entradatexto.text()
 
-        encontrar_volcan = []
+        encontrarVolcan = []
         for volcan in data:
-            if criteria == "Nombre del volcán" and volcan["Volcano Name"].lower() == value.lower():
-                encontrar_volcan.append(volcan)
-            elif criteria == "Región" and volcan["Region"].lower() == value.lower():
-                encontrar_volcan.append(volcan)
-            elif criteria == "Año" and value == volcan["Start Date"].split("-")[2]:
-                encontrar_volcan.append(volcan)
-            elif criteria == "VEI" and value == volcan["Max. VEI"]:
-                encontrar_volcan.append(volcan)
+            if criterio == "Nombre del volcán" and volcan["Volcano Name"].lower() == valor.lower():
+                encontrarVolcan.append(volcan)
+            elif criterio == "Región" and volcan["Region"].lower() == valor.lower():
+                encontrarVolcan.append(volcan)
+            elif criterio == "Año" and valor == volcan["Start Date"].split("-")[2]:
+                encontrarVolcan.append(volcan)
+            elif criterio == "VEI" and valor == volcan["Max. VEI"]:
+                encontrarVolcan.append(volcan)
 
-        self.mostrar_resultados(encontrar_volcan)
+        self.mostrarResultados(encontrarVolcan)
 
-    def mostrar_resultados(self, volcanes):
-        self.result_table.setRowCount(len(volcanes))
+    def mostrarResultados(self, volcanes):
+        self.resultadoTabla.setRowCount(len(volcanes))
 
         for index, volcan in enumerate(volcanes):
-            region_item = QTableWidgetItem(volcan["Region"])
-            nombre_item = QTableWidgetItem(volcan["Volcano Name"])
-            año_item = QTableWidgetItem(volcan["Start Date"].split("-")[2])
-            vei_item = QTableWidgetItem(volcan["Max. VEI"])
+            regionItem = QTableWidgetItem(volcan["Region"])
+            nombreItem = QTableWidgetItem(volcan["Volcano Name"])
+            añoItem = QTableWidgetItem(volcan["Start Date"].split("-")[2])
+            veiItem = QTableWidgetItem(volcan["Max. VEI"])
 
             coordenadas = volcan.get("Latitude (dd)", "") + ", " + volcan.get("Longitude (dd)", "")
-            coordenadas_item = QTableWidgetItem(coordenadas)
+            coordenadasItem = QTableWidgetItem(coordenadas)
 
-            self.result_table.setItem(index, 0, region_item)
-            self.result_table.setItem(index, 1, nombre_item)
-            self.result_table.setItem(index, 2, año_item)
-            self.result_table.setItem(index, 3, vei_item)
-            self.result_table.setItem(index, 4, coordenadas_item)
+            self.resultadoTabla.setItem(index, 0, regionItem)
+            self.resultadoTabla.setItem(index, 1, nombreItem)
+            self.resultadoTabla.setItem(index, 2, añoItem)
+            self.resultadoTabla.setItem(index, 3, veiItem)
+            self.resultadoTabla.setItem(index, 4, coordenadasItem)
 
-        self.result_table.resizeColumnsToContents()
+        self.resultadoTabla.resizeColumnsToContents()
 
 
 if __name__ == "__main__":
