9995be2098a7e1bd5f35398bcbc031a1fc66d25e
pipelinioo <129526062+pipelinioo@users.noreply.github.com>
2023-06-20 20:40:38 -0400

    unos committt

--- busqueda.py
+++ busqueda.py
@@ -1,172 +1,116 @@
-import csv
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, QComboBox
+import os
 import sys
+import csv
+from PyQt6.QtWidgets import QApplication, QMainWindow, QComboBox, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem
+
+csv_file_path = os.path.abspath(r'C:\Users\fidja\Downloads\Volcanes-EDD-main\Volcanes-EDD-main\erupcionesdesde1903.csv')
 
 data = []
-with open('erupciones.csv', 'r', encoding='utf-8-sig') as file:
+with open(csv_file_path, 'r', encoding='utf-8-sig') as file:
     reader = csv.DictReader(file, delimiter=';')
     for row in reader:
         data.append(row)
 
-class VentanaDeBusqueda(QMainWindow):
+class VentanaBusqueda(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Búsqueda de Erupciones Volcánicas")
-        self.setGeometry(300, 300, 400, 300)
+        self.setWindowTitle("Búsqueda de Volcanes")
+        self.setGeometry(100, 100, 600, 400)
 
-        self.central_widget = QWidget()
-        self.layout = QVBoxLayout()
-        self.central_widget.setLayout(self.layout)
+        self.combobox = QComboBox(self)
+        self.combobox.addItems(["Nombre del volcán", "Región", "Año", "VEI"])
+        self.combobox.setCurrentIndex(0)
+        self.combobox.currentIndexChanged.connect(self.combobox_region)
+        self.combobox.move(50, 30)
 
-        self.text_resultados = QTextEdit()
-        self.layout.addWidget(self.text_resultados)
+        self.input_entry = QLineEdit(self)
+        self.input_entry.move(50, 70)
 
-        self.setCentralWidget(self.central_widget)
+        self.region_combobox = QComboBox(self)
+        self.region_combobox.addItems(
+            [
+                "Region de Arica y Parinacota",
+                "Region de Tarapaca",
+                "Region de Antofagasta",
+                "Region de Atacama",
+                "Region de Coquimbo",
+                "Region de Valparaiso",
+                "Region Metropolitana",
+                "Region de O'Higgins",
+                "Region del Maule",
+                "Region de Nuble",
+                "Region del Bio Bio",
+                "Region de La Araucania",
+                "Region de Los Rios",
+                "Region de Los Lagos",
+                "Region de Aysen",
+                "Region de Magallanes y la Antartida Chilena",
+            ]
+        )
+        self.region_combobox.setCurrentIndex(0)
+        self.region_combobox.move(50, 70)
+        self.region_combobox.hide()
 
-        self.combobox = QComboBox()
-        self.combobox.addItem("Región")
-        self.combobox.addItem("Nombre del volcán")
-        self.combobox.addItem("Año")
-        self.combobox.addItem("VEI")
-        self.combobox.setCurrentIndex(-1)
-        self.layout.addWidget(self.combobox)
+        self.search_button = QPushButton("Buscar", self)
+        self.search_button.clicked.connect(self.realizar_busqueda)
+        self.search_button.move(50, 110)
 
-        self.combobox.currentIndexChanged.connect(self.comboBoxSegunda)
+        self.result_table = QTableWidget(self)
+        self.result_table.setGeometry(50, 150, 500, 240)
+        self.result_table.setColumnCount(5)
+        self.result_table.setHorizontalHeaderLabels(["Región", "Nombre del volcán", "Año", "VEI", "Coordenadas"])
 
-    def comboBoxSegunda(self, index):
-        while self.layout.count() > 1:
-            item = self.layout.takeAt(1)
-            widget = item.widget()
-            if widget is not None:
-                widget.deleteLater()
-
-        if index == 0:
-            region_combobox = QComboBox()
-            region_combobox.setCurrentIndex(-1)
-            region_combobox.addItem("Region de Arica y Parinacota")
-            region_combobox.addItem("Region de Tarapacá")
-            region_combobox.addItem("Region de Antofagasta")
-            region_combobox.addItem("Region de Atacama")
-            region_combobox.addItem("Region de Coquimbo")
-            region_combobox.addItem("Region de Valparaíso")
-            region_combobox.addItem("Region de Metropolitana")
-            region_combobox.addItem("Region de O'Higgins")
-            region_combobox.addItem("Region del Maule")
-            region_combobox.addItem("Region de Ñuble")
-            region_combobox.addItem("Region de Biobío")
-            region_combobox.addItem("Region de La Araucanía")
-            region_combobox.addItem("Region de Los Ríos")
-            region_combobox.addItem("Region de Los Lagos")
-            region_combobox.addItem("Region de Aysén")
-            region_combobox.addItem("Region de Magallanes")
-            boton_buscar = QPushButton("Buscar")
-            self.layout.addWidget(region_combobox)
-            self.layout.addWidget(boton_buscar)
-            boton_buscar.clicked.connect(lambda: self.realizarBusqueda("Region", region_combobox.currentText()))
-
-        elif index == 1:
-            self.label_busqueda = QLabel("Buscar Volcán:")
-            self.layout.addWidget(self.label_busqueda)
-
-            self.entry_busqueda = QLineEdit()
-            self.layout.addWidget(self.entry_busqueda)
-
-            self.btn_buscar = QPushButton("Buscar")
-            self.btn_buscar.clicked.connect(self.buscar_erupcion)
-            self.layout.addWidget(self.btn_buscar)
-
-        elif index == 2:
-            label = QLabel("Año:")
-            self.layout.addWidget(label)
-
-            line_edit = QLineEdit()
-            self.layout.addWidget(line_edit)
-
-            search_button = QPushButton("Buscar")
-            search_button.clicked.connect(lambda: self.realizarBusqueda("Start Date", line_edit.text()))
-            self.layout.addWidget(search_button)
-
-        elif index == 3:
-            vei_combobox = QComboBox()
-            vei_combobox.addItem("0")
-            vei_combobox.addItem("1")
-            vei_combobox.addItem("2")
-            vei_combobox.addItem("3")
-            vei_combobox.addItem("4")
-            vei_combobox.addItem("5")
-            vei_combobox.addItem("6")
-            vei_combobox.addItem("7")
-            vei_combobox.addItem("8")
-            search_button = QPushButton("Buscar")
-            self.layout.addWidget(vei_combobox)
-            self.layout.addWidget(search_button)
-            search_button.clicked.connect(lambda: self.realizarBusqueda("Max. VEI", vei_combobox.currentText()))
-
-    def buscar_erupcion(self):
-        busqueda = self.entry_busqueda.text()
-        resultados = []
-        for row in data:
-            if busqueda.lower() in row['Volcano Name'].lower():
-                resultados.append(row)
-        self.mostrar_resultados(resultados)
+    def combobox_region(self, index):
+        if index == 1:
+            self.input_entry.hide()
+            self.region_combobox.show()
+        else:
+            self.region_combobox.hide()
+            self.input_entry.show()
 
     def realizar_busqueda(self):
-            criteria = self.combobox.currentText()
+        criteria = self.combobox.currentText()
+        if criteria == "Región":
+            value = self.region_combobox.currentText()
+        else:
             value = self.input_entry.text()
 
-            try:
-                with open("erupciones.csv", "r", encoding="latin-1") as file:
-                    reader = csv.DictReader(file, delimiter=";")
-                    encontrar_volcan = []
-                    for row in reader:
-                        if criteria == "Región" and row["Region"].lower() == value.lower():
-                            encontrar_volcan.append(row)
-                        elif criteria == "Nombre del volcán" and row["Volcano Name"].lower() == value.lower():
-                            encontrar_volcan.append(row)
-                        elif criteria == "Año" and row["Start Date"] == value:
-                            encontrar_volcan.append(row)
-                        elif criteria == "VEI" and row["Max. VEI"] == value:
-                            encontrar_volcan.append(row)
+        encontrar_volcan = []
+        for volcan in data:
+            if criteria == "Nombre del volcán" and volcan["Volcano Name"].lower() == value.lower():
+                encontrar_volcan.append(volcan)
+            elif criteria == "Región" and volcan["Region"].lower() == value.lower():
+                encontrar_volcan.append(volcan)
+            elif criteria == "Año" and value == volcan["Start Date"].split("-")[2]:
+                encontrar_volcan.append(volcan)
+            elif criteria == "VEI" and value == volcan["Max. VEI"]:
+                encontrar_volcan.append(volcan)
 
-                if encontrar_volcan:
-                    result_text = "Resultados de la búsqueda:\n\n"
-                    for volcan in encontrar_volcan:
-                        result_text += "-" * 30 + "\n"
-                        result_text += "Región: {}\n".format(volcan["Region"])
-                        result_text += "Nombre del volcán: {}\n".format(volcan["Volcano Name"])
-                        result_text += "Año: {}\n".format(volcan["Start Date"])
-                        result_text += "VEI: {}\n".format(volcan["Max. VEI"])
-                        result_text += "Coordenadas: {}\n".format(volcan["Latitude"] + ", " + volcan["Longitude"])
-                        result_text += "\n"
+        self.mostrar_resultados(encontrar_volcan)
 
-                    self.result_textedit.setText(result_text)
-                else:
-                    self.result_textedit.setText("No se encontraron volcanes que coincidan con los criterios de búsqueda.")
+    def mostrar_resultados(self, volcanes):
+        self.result_table.setRowCount(len(volcanes))
 
-            except FileNotFoundError:
-                self.result_textedit.setText("No se encontró el archivo 'erupcionesdesde1903v2.csv'.")
+        for index, volcan in enumerate(volcanes):
+            region_item = QTableWidgetItem(volcan["Region"])
+            nombre_item = QTableWidgetItem(volcan["Volcano Name"])
+            año_item = QTableWidgetItem(volcan["Start Date"].split("-")[2])
+            vei_item = QTableWidgetItem(volcan["Max. VEI"])
 
-    def mostrar_resultados(self, resultados):
-        self.text_resultados.clear()
+            coordenadas = volcan.get("Latitude (dd)", "") + ", " + volcan.get("Longitude (dd)", "")
+            coordenadas_item = QTableWidgetItem(coordenadas)
 
-        if len(resultados) == 0:
-            self.text_resultados.setPlainText("No se encontraron resultados.")
-        else:
-            for row in resultados:
-                texto = "Volcano Name: {}\nVolcano Name Ref: {}\nRegion: {}\nStart Date: {}\nMax. VEI: {}\nLatitude: {}\nLongitude: {}\n\n".format(
-                    row.get('Volcano Name', ''),
-                    row.get('Volcano Name Ref', ''),
-                    row.get('Region', ''),
-                    row.get('Start Date', ''),
-                    row.get('Max. VEI', ''),
-                    row.get('Latitude', ''),
-                    row.get('Longitude', '')
-                )
-                self.text_resultados.insertPlainText(texto)
+            self.result_table.setItem(index, 0, region_item)
+            self.result_table.setItem(index, 1, nombre_item)
+            self.result_table.setItem(index, 2, año_item)
+            self.result_table.setItem(index, 3, vei_item)
+            self.result_table.setItem(index, 4, coordenadas_item)
+
+        self.result_table.resizeColumnsToContents()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ventana = VentanaDeBusqueda()
+    ventana = VentanaBusqueda()
     ventana.show()
     sys.exit(app.exec())
-
