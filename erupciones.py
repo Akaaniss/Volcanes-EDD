8f03ee2b2244e4bac17c88baaf175f229ac79fa9
pipelinioo <felipeignacio.delgado@alumnos.ulagos.cl>
2023-07-11 16:21:54 -0400

    realiza busqueda en el otro csv, excepto con año, tampoco realiza la busqueda de año en el csv inicial

--- erupciones.py
+++ erupciones.py
@@ -5,27 +5,31 @@ import matplotlib.pyplot as plt
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QComboBox, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-relative_path = os.path.join(current_dir, "erupcionesdesde1903.csv")
-
-data = []
-with open(relative_path, 'r', encoding='utf-8-sig') as file:
-    reader = csv.DictReader(file, delimiter=';')
-    for row in reader:
-        data.append(row)
-
-
 class VentanaBusqueda(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Búsqueda de Volcanes")
         self.setGeometry(150, 150, 580, 560)
 
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.relative_path = os.path.join(self.current_dir, "erupcionesdesde1903.csv")
+
+        self.data = []
         self.archivo_csv_actual = "erupcionesdesde1903.csv"  # archivo actual
 
+        self.load_data()
+        self.create_widgets()
+
+    def load_data(self):
+        with open(self.archivo_csv_actual, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file, delimiter=';')
+            for row in reader:
+                self.data.append(row)
+
+    def create_widgets(self):
         self.combobox = QComboBox(self)
         self.combobox.addItems(["Nombre del volcán", "Región", "Año", "VEI"])
-        self.combobox.setCurrentIndex(0)
+        self.combobox.setCurrentIndex(1)
         self.combobox.currentIndexChanged.connect(self.comboboxRegion)
         self.combobox.move(50, 30)
         self.combobox.setFixedWidth(250)
@@ -35,30 +39,11 @@ class VentanaBusqueda(QMainWindow):
         self.entradatexto.setFixedWidth(250)
 
         self.regionCombobox = QComboBox(self)
-        self.regionCombobox.addItems(
-            [
-                "Region de Arica y Parinacota",
-                "Region de Tarapaca",
-                "Region de Antofagasta",
-                "Región de Atacama",
-                "Region de Coquimbo",
-                "Region de Valparaiso",
-                "Region Metropolitana",
-                "Region de O'Higgins",
-                "Region del Maule",
-                "Region de Nuble",
-                "Region del Biobio",
-                "Region de La Araucania",
-                "Region de Los Rios",
-                "Region de Los Lagos",
-                "Region de Aysen",
-                "Region de Magallanes y la Antartica Chilena",
-            ]
-        )
+        self.regionCombobox.addItems(["Region de Arica y Parinacota", "Region de Tarapaca", "Region de Antofagasta", "Región de Atacama", "Region de Coquimbo", "Region de Valparaiso", "Region Metropolitana", "Region de O'Higgins", "Region del Maule", "Region de Nuble", "Region del Biobio", "Region de La Araucania", "Region de Los Rios", "Region de Los Lagos", "Region de Aysen", "Region de Magallanes y la Antartica Chilena"])
         self.regionCombobox.setCurrentIndex(0)
         self.regionCombobox.move(50, 70)
         self.regionCombobox.setFixedWidth(250)
-        self.regionCombobox.hide()
+        self.regionCombobox.setVisible(False)  # Ocultar la comboBox de regiones al inicio
 
         self.botonBusqueda = QPushButton("Buscar", self)
         self.botonBusqueda.clicked.connect(self.realizarBusqueda)
@@ -76,8 +61,6 @@ class VentanaBusqueda(QMainWindow):
 
         self.resultadoTabla = QTableWidget(self)
         self.resultadoTabla.setGeometry(50, 150, 520, 400)
-        self.resultadoTabla.setColumnCount(5)
-        self.resultadoTabla.setHorizontalHeaderLabels(["Región", "Nombre del volcán", "Año", "VEI", "Coordenadas"])
 
         self.mensajeLabel = QLabel(self)
         self.mensajeLabel.setGeometry(50, 150, 500, 30)
@@ -89,13 +72,23 @@ class VentanaBusqueda(QMainWindow):
         self.comboboxArchivo.currentIndexChanged.connect(self.cambiarArchivo)
         self.comboboxArchivo.move(320, 30)
         self.comboboxArchivo.setFixedWidth(250)
+
         opciones_erupciones = ["Nombre del volcán", "Región", "Año", "VEI"]
         opciones_volcano_data = ["Nombre del volcán", "País", "Año"]
 
         self.opciones_csv = {
-            "erupcionesdesde1903.csv": opciones_erupciones,
-            "volcano_data_2010.csv": opciones_volcano_data
-        }
+        "erupcionesdesde1903.csv": opciones_erupciones,
+        "volcano_data_2010.csv": opciones_volcano_data
+    }
+        self.columnas_csv = {
+        "erupcionesdesde1903.csv": ["Región", "Nombre del volcán", "Año", "VEI", "Coordenadas"],
+        "volcano_data_2010.csv": ["Nombre del volcán", "País", "Año","Locacion", "Latitude", "Longitude"]
+    }
+
+        self.actualizarComboBox()
+        self.actualizarDatos()
+        self.realizarBusqueda()
+
 
     def actualizarComboBox(self):
         opciones = self.opciones_csv.get(self.archivo_csv_actual, [])
@@ -107,6 +100,16 @@ class VentanaBusqueda(QMainWindow):
         else:
             self.regionCombobox.hide()
             self.entradatexto.show()
+        columnas_tabla = self.columnas_csv.get(self.archivo_csv_actual, [])
+        self.resultadoTabla.setColumnCount(len(columnas_tabla))
+        self.resultadoTabla.setHorizontalHeaderLabels(columnas_tabla)
+        if self.archivo_csv_actual == "volcano_data_2010.csv":
+            self.botonGrafico.hide()
+            self.botonGraficoVEI.hide()
+        else:
+            self.botonGrafico.show()
+            self.botonGraficoVEI.show()
+
 
     def cambiarArchivo(self, index):
         archivos_csv = ["erupcionesdesde1903.csv", "volcano_data_2010.csv"]  # Agrega los nombres de tus archivos CSV aquí
@@ -114,22 +117,27 @@ class VentanaBusqueda(QMainWindow):
         self.actualizarDatos()
         self.actualizarComboBox()
         self.realizarBusqueda()
-
+        if self.archivo_csv_actual == "volcano_data_2010.csv":
+            self.botonGrafico.hide()
+            self.botonGraficoVEI.hide()
+        else:
+            self.botonGrafico.show()
+            self.botonGraficoVEI.show()
     def actualizarDatos(self):
-        data = []
+        self.data = []
         with open(self.archivo_csv_actual, 'r', encoding='utf-8-sig') as file:
             reader = csv.DictReader(file, delimiter=';')
             for row in reader:
-                data.append(row)
-        self.datos = data
+                self.data.append(row)
+        self.mostrarResultados([])  # Limpiar la tabla de resultados
 
     def comboboxRegion(self, index):
         criterio = self.combobox.currentText()
         if self.archivo_csv_actual == "erupcionesdesde1903.csv" and criterio == "Región":
             self.entradatexto.hide()
-            self.regionCombobox.show()
+            self.regionCombobox.setVisible(True)  # Mostrar la comboBox de regiones si se selecciona "Región"
         else:
-            self.regionCombobox.hide()
+            self.regionCombobox.setVisible(False)  # Ocultar la comboBox de regiones en cualquier otro caso
             self.entradatexto.show()
 
     def realizarBusqueda(self):
@@ -153,7 +161,7 @@ class VentanaBusqueda(QMainWindow):
         encontrarVolcan = []
 
         if criterio == "Nombre del volcán" or criterio == "Región":
-            sorted_data = sorted(data, key=lambda x: x[criterio])
+            sorted_data = sorted(self.data, key=lambda x: x[criterio])
 
             if criterio == "Región":
                 left = 0
@@ -197,18 +205,34 @@ class VentanaBusqueda(QMainWindow):
                     else:
                         right = mid - 1
 
-        self.mostrarResultados(encontrarVolcan)
+        if self.archivo_csv_actual == "erupcionesdesde1903.csv":
+            self.mostrarResultados(encontrarVolcan)
+        elif self.archivo_csv_actual == "volcano_data_2010.csv":
+            self.mostrarResultadosDATA(encontrarVolcan)
 
     def BusquedaLineal(self, criterio, valor):
         encontrarVolcan = []
 
-        for item in data:
+        for item in self.data:
             if criterio == "Año" and item["Start Date"].split("-")[0] == valor:
                 encontrarVolcan.append(item)
             elif criterio == "VEI" and item["Max. VEI"] == valor:
                 encontrarVolcan.append(item)
 
         self.mostrarResultados(encontrarVolcan)
+
+    def BusquedaLinealVolcanoData(self, criterio, valor):
+        encontrarVolcan = []
+
+        for item in self.data:
+            if criterio == "Nombre del volcán" and item["Nombre del volcán"] == valor:
+                encontrarVolcan.append(item)
+            elif criterio == "País" and item["País"] == valor:
+                encontrarVolcan.append(item)
+            elif criterio == "Año" and item["Anio"] == valor:
+                encontrarVolcan.append(item)
+
+        self.mostrarResultadosDATA(encontrarVolcan)
 
     def mostrarResultados(self, resultados):
         if resultados:
@@ -220,6 +244,25 @@ class VentanaBusqueda(QMainWindow):
                 self.resultadoTabla.setItem(row, 2, QTableWidgetItem(item["Start Date"]))
                 self.resultadoTabla.setItem(row, 3, QTableWidgetItem(item["Max. VEI"]))
                 self.resultadoTabla.setItem(row, 4, QTableWidgetItem(f"{item['Latitude']}, {item['Longitude']}"))
+        else:
+            self.resultadoTabla.clearContents()
+            self.resultadoTabla.setRowCount(0)
+            self.resultadoTabla.setVisible(False)
+            self.mensajeLabel.setText("No se encontraron resultados.")
+
+        self.resultadoTabla.resizeColumnsToContents()
+
+    def mostrarResultadosDATA(self, resultados):
+        if resultados:
+            self.resultadoTabla.setVisible(True)
+            self.resultadoTabla.setRowCount(len(resultados))
+            for row, item in enumerate(resultados):
+                self.resultadoTabla.setItem(row, 0, QTableWidgetItem(item["Nombre del volcán"]))
+                self.resultadoTabla.setItem(row, 1, QTableWidgetItem(item["País"]))
+                self.resultadoTabla.setItem(row, 3,QTableWidgetItem(item["Locación"]))
+                self.resultadoTabla.setItem(row, 2, QTableWidgetItem(item["Anio"]))
+                self.resultadoTabla.setItem(row, 4, QTableWidgetItem(item["Latitude"]))
+                self.resultadoTabla.setItem(row, 5, QTableWidgetItem(item["Longitude"]))
             self.mensajeLabel.setText("")
         else:
             self.resultadoTabla.clearContents()
@@ -231,7 +274,7 @@ class VentanaBusqueda(QMainWindow):
 
     def generarGraficoVEI(self):
         magnitudes = []
-        for item in data:
+        for item in self.data:
             magnitudes.append(float(item["Max. VEI"]))
 
         plt.figure(figsize=(8, 6))
@@ -244,7 +287,7 @@ class VentanaBusqueda(QMainWindow):
     def generarGrafico(self):
         anos = []
         explosions_per_year = {}
-        for item in data:
+        for item in self.data:
             year = item["Start Date"].split("-")[2]
             if year in explosions_per_year:
                 explosions_per_year[year] += 1
@@ -258,37 +301,6 @@ class VentanaBusqueda(QMainWindow):
         plt.xticks(rotation=60)  # rota los años en 60 gradoss para que se vean mejor
         plt.tight_layout()
         plt.show()
-
-    def realizarBusqueda(self):
-        criterio = self.combobox.currentText()
-
-        if criterio == "Nombre del volcán" or criterio == "Región":
-            if criterio == "Región":
-                valor = self.regionCombobox.currentText()
-            else:
-                valor = self.entradatexto.text()
-            self.BusquedaBinaria(criterio, valor)
-        elif criterio == "Año" or criterio == "VEI":
-            valor = self.entradatexto.text()
-
-            if self.archivo_csv_actual == "volcano_data_2010.csv":
-                self.BusquedaLinealVolcanoData(criterio, valor)
-            else:
-                self.BusquedaLineal(criterio, valor)
-
-    def BusquedaLinealVolcanoData(self, criterio, valor):
-        encontrarVolcan = []
-
-        for item in data:
-            if criterio == "Nombre del volcán" and item["Nombre del volcan"] == valor:
-                encontrarVolcan.append(item)
-            elif criterio == "País" and item["Pais"] == valor:
-                encontrarVolcan.append(item)
-            elif criterio == "Año" and item["Ano"] == valor:
-                encontrarVolcan.append(item)
-
-        self.mostrarResultados(encontrarVolcan)
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
