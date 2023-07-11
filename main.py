0e81813fe8de9c0e278baccdc40f0fe8cb2fae12
pipelinioo <felipeignacio.delgado@alumnos.ulagos.cl>
2023-07-11 13:43:47 -0400

    a

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from busqueda import ventanadeBusqueda
-from PyQt6.QtWidgets import QApplication
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ventana = VentanaBusqueda()
-    ventana.show()
-    sys.exit(app.exec())
